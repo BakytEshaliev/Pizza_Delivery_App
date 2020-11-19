ee939aa707842a2014dcfe1003cfad34c47bd3ce
Ataibek Isakov <Ataibek.Isakov.kx4@fh-zwickau.de>
2020-11-19 23:00:50 +0100

    init commit for pizza

---
+++ Pizza_Isakov_Eshaliev/src/main/java/whz/pti/pizza/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package whz.pti.pizza.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import whz.pti.pizza.demo.service.TestService;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    @Autowired
+    TestService testService;
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(){
+        return (exp)->{
+            testService.test();
+        };
+    }
+}
